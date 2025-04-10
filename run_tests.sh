4551df692268a302e69361176abe8c8c77c8b099
kazu-321 <kzs321kzs@gmail.com>
2025-04-10 13:41:49 +0900

    feat: emoji

--- run_tests.sh
+++ run_tests.sh
@@ -24,7 +24,6 @@ if [ ! -d "$testdir" ]; then
     exit 1
 fi
 
-# echo "🔧 Compiling $cfile ..."
 exe="${cfilename}.out"
 gcc "$cfile" -o "$exe"
 
@@ -52,7 +51,7 @@ for input in "$testdir"/input*.txt; do
     ./"$exe" < "$input" > temp_output.txt
 
     if diff -q temp_output.txt "$expected" > /dev/null; then
-        echo "## Test $num passed"
+        echo "## ✅ Test $num passed"
         echo "### 入力:"
         echo '```'
         echo $(cat "$input")
@@ -64,7 +63,7 @@ for input in "$testdir"/input*.txt; do
         echo '```'
         ((pass++))
     else
-        echo "## Test $num failed"
+        echo "## ❌ Test $num failed"
         echo "### 入力:"
         echo '```'
         echo "$(cat "$input")"
