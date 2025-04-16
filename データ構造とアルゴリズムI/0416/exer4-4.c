853e0ba40dc48a8a6dbeaf26e2c8a83202349906
kazu-321 <kzs321kzs@gmail.com>
2025-04-16 09:20:23 +0900

    add: exer4-4.c

---
+++ データ構造とアルゴリズムI/0416/exer4-4.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+int main() {
+    double a[10];
+    a[0] = 1;
+    a[1] = 2;
+    // 2a_{n+2} - 3a_{n+1} + a_n = 0
+    // 2a_{n} - 3a_{n-1} + a_{n-2} = 0
+    // 2a_{n} = 3a_{n-1} - a_{n-2}
+    // a_{n} = 1.5a_{n-1} - 0.5a_{n-2}
+    for (int i = 2; i < 10; i++) {
+        a[i] = a[i - 1] * 3. / 2. - a[i - 2] / 2.;
+    }
+    printf("%f\n", a[9]);
+    return 0;
+}
